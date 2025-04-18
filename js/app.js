ee6008d42d3c8592af40a825f71fa2e4fe6b1bd3
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2025-04-18 10:42:48 +0530

    game triggering is working fine to ending

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let userScore = 0
+const gameMaxTime = 20
+let gameScreenTime = gameMaxTime
+
+let timer = null
+
+const screenTimeDisplay = document.querySelector('.screen-time-display')
+const userScoreDisplay = document.querySelector('.user-score-display')
+
+const gameGrid = document.querySelector('#game-grid')
+const startGameBtn = document.querySelector('.start-game-btn')
+
+
+// game begins
+startGameBtn.addEventListener('click', ()=> {
+  gameTriggering()
+})
+
+function gameTriggering() {
+  timer = setInterval(timerRunning, 600)
+
+}
+
+
+function timerRunning() {
+  console.log('time running....')
+  screenTimeDisplay.textContent = gameScreenTime
+  gameScreenTime--
+  if (gameScreenTime <= 0) {
+    screenTimeDisplay.textContent = 'Times Up'
+    console.log('the game ends')
+    // timer killing
+    clearInterval(timer)
+  }
+}
